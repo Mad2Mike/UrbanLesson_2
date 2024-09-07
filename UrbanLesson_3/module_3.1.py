e5dd7d47b987b98921c6f6632f66da441ece5883
Mad2Mike <mad3mike@gmail.com>
2024-09-08 01:12:18 +0300

    Module 3 Lesson 1 and structure changes

---
+++ UrbanLesson_3/module_3.1.py
@@ -0,0 +1,29 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string: str):
+    global calls
+    calls += 1
+    st_ln = len(string)
+    st_up = string.upper()
+    st_dn = string.lower()
+    str_info = (st_ln, st_up, st_dn)
+    return str_info
+
+def is_contains(string: str, list_to_search: list):
+    global calls
+    calls += 1
+    if string.lower() in (i.lower() for i in list_to_search):
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
