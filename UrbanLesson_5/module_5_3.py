e225290a526dfe3dc354cbf9f1bca4f61463d7ba
Mad2Mike <mad3mike@gmail.com>
2024-10-07 23:04:28 +0300

    Module 5_3

---
+++ UrbanLesson_5/module_5_3.py
@@ -0,0 +1,86 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        if 1 < new_floor <= self.number_of_floors:
+            i = 1
+            while i <= new_floor:
+                print(i)
+                i += 1
+        else:
+            print("Такого этажа не существует")
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, (int, House)):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, (int, House)):
+            return self.number_of_floors < other.number_of_floors
+        else:
+            return
+    def __le__(self, other):
+        if isinstance(other, (int, House)):
+            return self.number_of_floors <= other.number_of_floors
+        else:
+            return
+    def __gt__(self, other):
+        if isinstance(other, (int, House)):
+            return self.number_of_floors > other.number_of_floors
+        else:
+            return
+    def __ge__(self, other):
+        if isinstance(other, (int, House)):
+            return self.number_of_floors >= other.number_of_floors
+        else:
+
+            return
+    def  __ne__(self, other):
+        if isinstance(other, (int, House)):
+            return self.number_of_floors != other.number_of_floors
+        else:
+
+            return
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        else:
+            return
+    def __radd__(self, value):
+        return self.__add__(value)
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+
+print(h2)
+
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
