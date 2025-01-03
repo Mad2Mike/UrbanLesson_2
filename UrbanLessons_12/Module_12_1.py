37a07f4ef2a7d25eb8af7e640880325786ef8062
Mad2Mike <mad3mike@gmail.com>
2025-01-03 19:12:40 +0300

    Module 12_1

---
+++ UrbanLessons_12/Module_12_1.py
@@ -0,0 +1,41 @@
+import unittest
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 200
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner_obj = Runner("TestRunnerObj")
+
+        for i in range(10):
+            runner_obj.walk()
+        self.assertEqual(runner_obj.distance, 50)
+
+    def test_run(self):
+        runner_obj = Runner("TestRunnerObj")
+        for _ in range(10):
+            runner_obj.run()
+        self.assertEqual(runner_obj.distance, 100)
+
+    def test_challenge(self):
+        runner_obj_1 = Runner("TestRunnerObj1")
+        runner_obj_2 = Runner("TestRunnerObj2")
+        for _ in range(10):
+            runner_obj_1.run()
+            runner_obj_2.walk()
+        self.assertNotEqual(runner_obj_1.distance, runner_obj_2.distance)
+
+if __name__ == '__main__':
+    unittest.main()
